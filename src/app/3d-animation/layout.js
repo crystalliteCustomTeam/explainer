0635330da4de2f847d53d132071041ad0d891ae7
Harry Kennedy <72253270+Harrythedevelopercs@users.noreply.github.com>
2024-08-23 03:55:18 +0500

    first commit

---
+++ src/app/3d-animation/layout.js
@@ -0,0 +1,23 @@
+//===== Meta Data =====
+export const metadata = {
+    //===== Meta Tags =====
+    title: "3d Animations - Cloud Studios Inc",
+    description: "Learn more about the passionate team driving innovation, precision, and storytelling mastery in every animated frame.",
+    //===== OG Tags =====
+    openGraph: {
+        title: '3d Animations - Cloud Studios Inc',
+        description: 'Learn more about the passionate team driving innovation, precision, and storytelling mastery in every animated frame.',
+        url: '/2d-animation',
+        siteName: 'Cloud Studios Inc',
+        locale: 'en_US',
+        type: 'website',
+    },
+    //===== Canonical =====
+    alternates: { canonical: '/2d-animation' },
+    //===== GEO Tags =====
+    other: {},
+}
+
+export default function RootLayout({ children }) {
+    return (children);
+}
---
+++ src/app/explainer-videos/layout.js
@@ -0,0 +1,23 @@
+//===== Meta Data =====
+export const metadata = {
+    //===== Meta Tags =====
+    title: "Explainer Videos - Cloud Studios Inc",
+    description: "Learn more about the passionate team driving innovation, precision, and storytelling mastery in every animated frame.",
+    //===== OG Tags =====
+    openGraph: {
+        title: 'Explainer Videos - Cloud Studios Inc',
+        description: 'Learn more about the passionate team driving innovation, precision, and storytelling mastery in every animated frame.',
+        url: '/explainer-videos',
+        siteName: 'Cloud Studios Inc',
+        locale: 'en_US',
+        type: 'website',
+    },
+    //===== Canonical =====
+    alternates: { canonical: '/explainer-videos' },
+    //===== GEO Tags =====
+    other: {},
+}
+
+export default function RootLayout({ children }) {
+    return (children);
+}
