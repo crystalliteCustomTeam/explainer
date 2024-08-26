ffe5a956f615206526912ab7374cfb353c485a5f
Aziz-khan88 <aziz.rehman@crystallitedigital.com>
2024-08-26 19:27:43 +0500

    copyright content changes

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -169,7 +169,7 @@ const Footer = () => {
         </div>
         <div className="flex gap-x-5 py-5 flex-wrap sm:flex-nowrap gap-y-2 sm:gap-y-0 items-center border-t border-[#000000]">
           <div className="basis-full sm:basis-1/2">
-            <p className="text-sm poppins text-black leading-6 text-center sm:text-left">© Copyright 2024 Explainer Videos LLC – A Brand of Infinity Animations</p>
+            <p className="text-sm poppins text-black leading-6 text-center sm:text-left">© Copyright 2024 Explainer Videos LLC – A Brand of Bhaoo Inc</p>
           </div>
           <div className="basis-full sm:basis-1/2">
             <ul className="flex items-center justify-center sm:justify-end gap-4">
