3487af1acfce5d7aec79cde732713ea193261f53
rabia-dev <dev.ra@productions-dev.com>
2024-08-29 20:13:53 +0500

    add chat button in header

--- src/app/lp/2D-animation-offer/components/header/header.jsx
+++ src/app/lp/2D-animation-offer/components/header/header.jsx
@@ -2,10 +2,18 @@ import logo from "media/logo.webp";
 import Image from "next/image";
 import styles from "./header.module.css"
 import Link from "next/link";
-
+import { PiWechatLogoFill } from "react-icons/pi";
+import { BiSolidPhoneCall } from "react-icons/bi";
 
 
 const HeaderLP = () => {
+    // Chat Code
+    const handleChatOpen = (e) => {
+        e.preventDefault();
+        if (typeof $zopim !== 'undefined' && $zopim.livechat && $zopim.livechat.window) {
+            $zopim.livechat.window.show();
+        }
+    };
     return (
         <section className={styles.headerSection}>
             <div className="xs:px-4 sm:px-8 lg:max-w-7xl mx-auto py-[15px]">
@@ -15,8 +23,11 @@ const HeaderLP = () => {
                             <Image priority src={logo} alt='logo' width={150} height={40} />
                         </div>
                     </div>
-                    <div className="my-auto text-right">
-                        <Link href="tel:1-833-666-6684" className={`${styles.button} text-[15px] sm:text-[18px] py-3 px-6`}>1-833-666-6684</Link>
+                    <div className="my-auto text-right flex items-end justify-end">
+                        <Link href="javascript:$zopim.livechat.window.show();" className={`${styles.button} text-[15px] sm:text-[18px] py-3 px-6 max-w-[150px] me-2 hidden md:block`}>Chat Now</Link>
+                        <Link href="tel:1-833-666-6684" className={`${styles.button} text-[15px] sm:text-[18px] py-3 px-6 hidden md:block`}>1-833-666-6684</Link>
+                        <Link href="javascript:$zopim.livechat.window.show();" className={`${styles.IconButton} py-3 me-4 block md:hidden`}><PiWechatLogoFill size={30} /></Link>
+                        <Link href="tel:1-833-666-6684" className={`${styles.IconButton}  py-3 me-4 block md:hidden`}><BiSolidPhoneCall size={30} /></Link>
                     </div>
                 </div>
             </div>
