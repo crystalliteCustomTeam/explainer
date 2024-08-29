417f6d305470b8e329924dea2c465beeba98d566
rabia-dev <dev.ra@productions-dev.com>
2024-08-29 20:44:32 +0500

    changes in pop-up

--- src/components/popup/Popup.jsx
+++ src/components/popup/Popup.jsx
@@ -10,6 +10,8 @@ import usePopup from '@/app/configs/store/Popup';
 import closeBtn from 'media/video-explainer/close-btn.svg'
 import popUpImage from "media/infinity-studio-lp-new/brandshine.webp"
 import arrowRed from "media/icons/arrow-red.webp"
+import { GrLinkNext } from "react-icons/gr";
+import { AiOutlineClose } from "react-icons/ai";
 
 const Popup = ({ }) => {
 
@@ -195,19 +197,21 @@ const Popup = ({ }) => {
             <Dialog open={popup} handler={popupHandle} className='popup h-full w-full' style={{ background: 'rgba(0, 0, 0, 0.4)', backdropFilter: 'blur(8px)' }}>
                 <section className='h-screen lg:w-full py-7 2xl:py-12 px-4 md:px-6 flex items-center justify-center'>
                     <div className="popupBg bg-[url('../../public/video-explainer/popupnew-bg.webp')] bg-[length:100%_100%] bg-center bg-no-repeat pt-8 pb-8 xl:pt-9 px-4 sm:px-6 lg:px-10 relative w-full lg:w-[850px] xl:w-[900px] h-auto rounded-xl">
-                        <div className='w-[40px] xl:w-[60px] h-[40px] xl:h-[60px] absolute top-[1%] right-[1%] flex items-center justify-center text-center rounded-[50%] border-0 cursor-pointer'>
-                            <Image priority onClick={popupHandle} src={closeBtn} className="w-full" alt='Infinity Animations' />
+                        <div className='w-[40px] xl:w-[60px] h-[40px] xl:h-[60px] absolute top-[1%] right-[1%] flex items-center justify-center text-center rounded-[50%] border-0 cursor-pointer '>
+                            <span className="flex items-center justify-center w-[25px] h-[25px] xl:w-[30px] xl:h-[30px] bg-[#0489B3]/40 backdrop-blur-2xl rounded-full p-2 ms-2 object-contain formbutton" onClick={popupHandle}>
+                                <AiOutlineClose priority size={40} color='white' />
+                            </span>
                         </div>
                         <div className="grid grid-cols-12">
                             <div className="col-span-12 lg:col-span-6">
                                 <div className="txt pt-5 sm:pt-0">
-                                    <p className="montserrat uppercase text-[17px] 2xl:text-[20px] leading-[30px] 2xl:leading-[30px] text-center text-white bg-white/20 backdrop-blur-2xl rounded-[20px] px-4 sm:w-6/12 lg:w-9/12 ">Fill This Form to Avail</p>
-                                    <h2 className="fontsfpromedium uppercase text-[25px] sm:text-[35px] tracking-normal font-bold leading-[30px] sm:leading-[45px] text-white pb-0 xl:py-2 2xl:py-3">70% <span className="text-[#FB2035]">Discounts</span></h2>
+                                    <p className="montserrat uppercase text-[17px] 2xl:text-[20px] leading-[30px] 2xl:leading-[30px] text-center text-white bg-[#0489B3]/40 backdrop-blur-2xl rounded-[20px] px-4 sm:w-6/12 lg:w-9/12 ">Fill This Form to Avail</p>
+                                    <h2 className="fontsfpromedium uppercase text-[25px] sm:text-[35px] tracking-normal font-bold leading-[30px] sm:leading-[45px] text-white pb-0 xl:py-2 2xl:py-3">70% <span className="text-[#0489B3]">Discounts</span></h2>
                                     <p className="text-[14px] sm:text-[16px] xl:text-[20px] text-white leading-[26px] xl:leading-[28px] montserrat pl-2 pb-3 xl:pb-4 2xl:pb-6">On Video Animation Services</p>
                                 </div>
                                 <form className="relative z-[99]">
                                     <div className="relative">
-                                        <input type="text" id="name" name="name" placeholder="Enter Your Name" className="montserrat text-[10px] xl:text-[12px] text-white w-full h-[35px] xl:h-[50px] bg-[#242424]/100 rounded-[6px] backdrop-blur-xl px-3 lg:px-4 mb-3 2xl:mb-4 focus:outline-0" onChange={handleDataChange} />
+                                        <input type="text" id="name" name="name" placeholder="Enter Your Name" className="montserrat text-[10px] xl:text-[12px] text-black w-full h-[35px] xl:h-[50px] bg-[#fff]/100 rounded-[6px] backdrop-blur-xl px-3 lg:px-4 mb-3 2xl:mb-4 focus:outline-0" onChange={handleDataChange} />
                                         {errors.name && (
                                             <span className="text-[12px] block p-2 font-sans font-medium text-primary-100 absolute left-0 bottom-[-10px]">
                                                 {errors.name}
@@ -215,7 +219,7 @@ const Popup = ({ }) => {
                                         )}
                                     </div>
                                     <div className="relative">
-                                        <input type="email" id="email" name="email" placeholder="Enter Email Address" className="montserrat text-[10px] xl:text-[12px] text-white w-full h-[35px] xl:h-[50px] bg-[#242424]/100 rounded-[6px] backdrop-blur-xl px-3 lg:px-4 mb-3 2xl:mb-4 focus:outline-0" onChange={handleDataChange} />
+                                        <input type="email" id="email" name="email" placeholder="Enter Email Address" className="montserrat text-[10px] xl:text-[12px] text-black w-full h-[35px] xl:h-[50px] bg-[#fff]/100 rounded-[6px] backdrop-blur-xl px-3 lg:px-4 mb-3 2xl:mb-4 focus:outline-0" onChange={handleDataChange} />
                                         {errors.email && (
                                             <span className="text-[12px] block p-2 font-sans font-medium text-primary-100 absolute left-0 bottom-[-10px]">
                                                 {errors.email}
@@ -223,7 +227,7 @@ const Popup = ({ }) => {
                                         )}
                                     </div>
                                     <div className="relative">
-                                        <input type="tel" id="phone" name="phone" minLength="10" maxLength="13" placeholder="Enter Phone" className="montserrat text-[10px] xl:text-[12px] text-white w-full h-[35px] xl:h-[50px] bg-[#242424]/100 rounded-[6px] backdrop-blur-xl px-3 lg:px-4 mb-3 2xl:mb-4 focus:outline-0" onChange={handleDataChange} />
+                                        <input type="tel" id="phone" name="phone" minLength="10" maxLength="13" placeholder="Enter Phone" className="montserrat text-[10px] xl:text-[12px] text-black w-full h-[35px] xl:h-[50px] bg-[#fff]/100 rounded-[6px] backdrop-blur-xl px-3 lg:px-4 mb-3 2xl:mb-4 focus:outline-0" onChange={handleDataChange} />
                                         {errors.phone && (
                                             <span className="text-[12px] block p-2 font-sans font-medium text-primary-100 absolute left-0 bottom-[-10px]">
                                                 {errors.phone}
@@ -231,13 +235,15 @@ const Popup = ({ }) => {
                                         )}
                                     </div>
                                     <div className="relative">
-                                        <textarea id="message" name="message" className="montserrat text-[12px] text-white w-full h-[95px] bg-[#242424]/100 rounded-[6px] backdrop-blur-xl px-3 lg:px-4 pt-3 2xl:mb-4 focus:outline-0 resize-none" placeholder="Message" onChange={handleDataChange}></textarea>
+                                        <textarea id="message" name="message" className="montserrat text-[12px] text-black w-full h-[95px] bg-[#fff]/100 rounded-[6px] backdrop-blur-xl px-3 lg:px-4 pt-3 2xl:mb-4 focus:outline-0 resize-none" placeholder="Message" onChange={handleDataChange}></textarea>
                                     </div>
-                                    <button className='bg-prime text text-[10px]-xl:white border-0 h-[40px] xl:h-[50px] px-3 2xl:px-6 rounded-md flex items-center gap-x-2 mt-2 ml-[4px]' onClick={handleFormSubmit} disabled={isDisabled}>
+                                    <button className='bg-[#0489B3] text text-[10px]-xl:white border-0 h-[40px] xl:h-[50px] px-3 2xl:px-6 rounded-md flex items-center gap-x-2 mt-2 ml-[4px]' onClick={handleFormSubmit} disabled={isDisabled}>
                                         <span className="text-[12px] md:text-[16px] xl:text-[18px] font-normal font-sans text-white">
                                             {formStatus}
                                         </span>
-                                        <Image priority className="flex items-center justify-center w-[25px] h-[25px] xl:w-[30px] xl:h-[30px] bg-white rounded-full p-2 ms-2 object-contain formbutton" src={arrowRed} alt="Infinity Animations " />
+                                        <span className="flex items-center justify-center w-[25px] h-[25px] xl:w-[30px] xl:h-[30px] bg-white rounded-full p-2 ms-2 object-contain formbutton">
+                                            <GrLinkNext priority size={40} color='#0489B3' />
+                                        </span>
                                     </button>
                                 </form>
                             </div>
