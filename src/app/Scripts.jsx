8a37a8ee026bbf1fb19da3aab4fcbb5402b40fd1
Aziz-khan88 <aziz.rehman@crystallitedigital.com>
2024-11-04 23:34:09 +0500

    chaat code change

--- src/app/Scripts.jsx
+++ src/app/Scripts.jsx
@@ -12,13 +12,18 @@ const Scripts = () => {
             // zenDesk.defer = true;
             // document.body.appendChild(zenDesk);
 
-            //======== Tawk.to Live Chat ========//
-            const tawkScript = document.createElement("script");
-            tawkScript.async = true;
-            tawkScript.src = 'https://embed.tawk.to/6724258c2480f5b4f5970d5f/1ibij5c37';
-            tawkScript.charset = 'UTF-8';
-            tawkScript.setAttribute('crossorigin', '*');
-            document.head.appendChild(tawkScript);
+            //======== LiveChat Integration ========//
+            window._lc = window._lc || {};
+            window.__lc = window.__lc || {};
+            window.__lc.license = 18818250;
+            window.__lc.integration_name = "manual_onboarding";
+            window.__lc.product_name = "livechat";
+
+            (function (n, t, c) {
+                function i(n) { return e.h ? e._h.apply(null, n) : e._q.push(n) }
+                var e = { _q: [], _h: null, _v: "2.0", on: function () { i(["on", c.call(arguments)]) }, once: function () { i(["once", c.call(arguments)]) }, off: function () { i(["off", c.call(arguments)]) }, get: function () { if (!e._h) throw new Error("[LiveChatWidget] You can't use getters before load."); return i(["get", c.call(arguments)]) }, call: function () { i(["call", c.call(arguments)]) }, init: function () { var n = t.createElement("script"); n.async = !0; n.type = "text/javascript"; n.src = "https://cdn.livechatinc.com/tracking.js"; t.head.appendChild(n) } };
+                !n._lc.asyncInit && e.init(), n.LiveChatWidget = n.LiveChatWidget || e;
+            })(window, document, [].slice);
 
 
             //======== Google Analytics ========//
